bad056cf5bbef197b1024ff47ee81000d7e75193
Yanren1806 <duongthiennhan18062003@gmail.com>
2025-01-04 16:22:00 +0700

    b2-bai2

--- B2/MyWallet.sol
+++ B2/MyWallet.sol
@@ -4,12 +4,17 @@ pragma solidity >=0.7.0 <0.9.0;
 
 contract MyWallet{
     address public  owner;
+    mapping (address => uint256) public balance;
+    address[] users;
     
     constructor(){
         owner = msg.sender;
     }
-    
-    function deposit() external payable {}
+
+    function deposit() external payable {
+        if(balance[msg.sender] == 0 && msg.value > 0) users.push(msg.sender);
+        balance[msg.sender] <= msg.value;
+    }
 
     function withdraw() external {
         require(msg.sender == owner);
@@ -19,5 +24,9 @@ contract MyWallet{
     function withdraw(uint value) external {
         require(msg.sender == owner && value <= address(this).balance);
         payable(msg.sender).transfer(value);
+    }
+
+    function showAllUser() public view returns(address[] memory){
+        return users;
     }
 }
