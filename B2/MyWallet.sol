f00d02d253fd76d74c3ffe2833c9c09300ba7c33
Yanren1806 <duongthiennhan18062003@gmail.com>
2025-01-04 16:42:56 +0700

    THEM DAU CONG

--- B2/MyWallet.sol
+++ B2/MyWallet.sol
@@ -13,15 +13,15 @@ contract MyWallet{
 
     function deposit() external payable {
         if(balance[msg.sender] == 0 && msg.value > 0) users.push(msg.sender);
-        balance[msg.sender] <= msg.value;
+        balance[msg.sender] += msg.value;
     }
 
-    function withdraw() external {
+    function withdrawll() external {
         require(msg.sender == owner);
         payable(msg.sender).transfer(address(this).balance);
     }
 
-    function withdraw(uint value) external {
+    function withdrawa(uint value) external {
         require(msg.sender == owner && value <= address(this).balance);
         payable(msg.sender).transfer(value);
     }
